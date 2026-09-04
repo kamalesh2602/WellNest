ece9eb2e43ec02cd8935eed4b50344cb5e90232a
kamalesh2602 <kamaleshg2602@gmail.com>
2026-09-04 10:33:00 +0530

    cors issue

--- server.js
+++ server.js
@@ -26,19 +26,7 @@ const allowedOrigins = [
     'http://127.0.0.1:5500' // VS Code Live Server
 ].filter(Boolean);
 
-app.use(cors({
-    origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps or curl)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) !== -1 || origin.startsWith('http://localhost') || origin.startsWith('http://127.0.0.1')) {
-            return callback(null, true);
-        } else {
-            return callback(new Error('Not allowed by CORS policy'));
-        }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true
-}));
+app.use(cors());
 
 // Middlewares
 app.use(express.json());
